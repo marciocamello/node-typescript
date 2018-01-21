7b63f0d40eb31ad65297f1d85d1f67320a8e5310
marciocamello@outlook.com <netunia234>
2018-01-20 22:19:38 -0200

    add ejs template

--- src/App.ts
+++ src/App.ts
@@ -1,19 +1,20 @@
 import * as express from 'express'
+import * as path from "path"
 
 class App {
     public express;
 
     constructor () {
         this.express = express();
+        this.express.set("views", path.join(__dirname, "../views"));
+        this.express.set("view engine", "ejs");
         this.mountRoutes()
     }
 
     private mountRoutes (): void {
         const router = express.Router();
         router.get('/', (req, res) => {
-            res.json({
-                message: 'Hello World!'
-            })
+            res.render('index');
         });
         this.express.use('/', router)
     }
